0d9519a3c298e9f5c7844f395f200ec2a9690f40
SandyBhai03 <kingsamrat94543@gmail.com>
2025-03-13 17:14:17 +0530

    modified index.html title  with icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ToDoList from './components/ToDoList';
+import React from "react";
+import ToDoList from "./components/ToDoList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="bg-stone-900 grid py-4 min-h-screen">
+      <Header />
       <ToDoList />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="text-center">
+      {/* <h1 className="text-2xl text-center font-bold text-red-400">Live Link</h1> */}
+      <p className="text-blue-700 font-semibold border-1 w-fit mx-auto m-1 py-1 px-2 bg-white hover:bg-slate-300 rounded">
+        <a href="https://sandybhai03.github.io/ReactToDo/">Click Me! See the Magic</a>
+      </p>
+    </div>
   )
 }
 
